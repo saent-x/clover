c40249a2794c8db3ebc42238076fd2e42af275b9
edosakelvin@gmail.com <edosakelvin@gmail.com>
2018-11-02 20:44:53 +0100

    Implemented identification of pages

--- index.js
+++ index.js
@@ -1,3 +1,79 @@
-const pageTypeMarkers = {
-    
+const cheerio = require("cheerio");
+
+function CreateMarker(tag, matcher) {
+    if (!tag) {
+        throw new Error("Cannot create matcher with invalid tag");
+    }
+    return {
+        tag,
+        matcher,
+        /* test a string using the specified matcher */
+        test: function(html) {
+            return this.matcher.test(html);
+        }
+    }
 }
+
+const getParser = (type) => {
+    if (parsers && parsers.length > 0) {
+        for (const parser in parsers) {
+            if (parser.for === type) {
+                return parser.parse;
+            }
+        }
+    }
+    else {
+        throw new Error("No parser found for page with type " + type);
+    }
+}
+
+
+/* setup an array of markers used to isdentify pages received from the
+ * server */
+const markers = [
+    CreateMarker("session", /\test/gi),
+    CreateMarker("auth-failed", /\WAuthentication Failed\W/gi),
+    CreateMarker("invalid-request", /\WInvalid Request\W/gi),
+    CreateMarker("confirm-override", /\Walready logged\W/gi)
+]
+
+/* when a parse fails to parse, it will return null or undefined as 
+ * opposed to throwing an Error. This behaviour may change in the future */
+const parsers = [
+    {
+        for: "session",
+        parse: (html) => {
+            /* parsing logic here */
+        },
+
+        for: "confirm-override",
+        parse: (html) => {
+            /* parsing logic here */
+        }
+    }
+]
+
+function parse(html) {
+    if (!html) {
+        return null;
+    }
+
+    const type = identify(html);
+    /* find the appropriate response generator based on the type */
+    return getParser(type)();
+
+}
+
+function identify(html) {
+    for (marker of markers) {
+        if (marker.test(html)) {
+            return marker.tag;
+        }
+    }
+}
+
+module.exports = {
+    identify,
+    parse
+}
+
