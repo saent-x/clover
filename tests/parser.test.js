c08cdf55ebdfc9e3f95007fbb929c53b362bffa5
edosakelvin@gmail.com <edosakelvin@gmail.com>
2018-11-09 01:35:41 +0100

    Made tests async

--- tests/parser.test.js
+++ tests/parser.test.js
@@ -1,31 +1,25 @@
 const { parse, pageTags } = require("../lib/parser");
 const { loadMockPage } = require("../lib/util");
 
-test("parse correct session id", () => {
-    return loadMockPage("session").then(page => {
-        expect(parse(page).data.Id).toBe(52438975);
-    })
-})
+test("parse correct session id", async () => {
+    const page = await loadMockPage("session");
+    expect(parse(page).data.Id).toBe(52438975);
+});
 
-
-describe('identify pages', () => {
+describe('identify pages', async () => {
     test("identify confirm-override page", () => {
-        return loadMockPage("confirm-override").then(page => {
-            expect(parse(page).type)
-                .toBe(pageTags.confirmOverride);
-        })
+        const page = await loadMockPage("confirm-override");
+        expect(parse(page).type).toBe(pageTags.confirmOverride);
+    });
+
+    test("identify auth-failed page", async () => {
+        const page = await loadMockPage("auth-failed");
+        expect(parse(page).type).toBe(pageTags.authFailed);
+    });
+
+    test("identify session page", async () => {
+        const page = await loadMockPage("session");
+        expect(parse(page).type).toBe(pageTags.session);
     })
-    test("identify auth-failed page", () => {
-        return loadMockPage("auth-failed").then(page => {
-            expect(parse(page).type)
-                .toBe(pageTags.authFailed);
-        })
-    })
-    test("identify session page", () => {
-        return loadMockPage("session").then(page => {
-            expect(parse(page).type)
-                .toBe(pageTags.session);
-        })
-    })
-})
+});
 
