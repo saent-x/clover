fcd38b6e2f3fea3ea0be840afb2f591140a476b6
edosakelvin@gmail.com <edosakelvin@gmail.com>
2018-11-12 19:38:23 +0100

    Reformated code

--- tests/parser.test.js
+++ tests/parser.test.js
@@ -4,25 +4,37 @@ const { loadMockPage } = require("../lib/util");
 const { tags } = require("../lib/Matcher");
 
 test("parse correct session id", async t => {
-  const parser = new Parser();
   const page = await loadMockPage("session");
-  t.is(parser.parse(page).data.id, 52438975);
+
+  const parser = new Parser();
+  const result = parser.parse(page);
+
+  t.is(result.data.id, 52438975);
 });
 
 test("identify confirm-override page", async t => {
-  const parser = new Parser();
   const page = await loadMockPage("confirm-override");
-  t.is(parser.parse(page).type, tags.confirmOverride);
+
+  const parser = new Parser();
+  const result = parser.parse(page);
+
+  t.is(result.type, tags.confirmOverride);
 });
 
 test("identify auth-failed page", async t => {
-  const parser = new Parser();
   const page = await loadMockPage("auth-failed");
-  t.is(parser.parse(page).type, tags.authenticationFailed);
+
+  const parser = new Parser();
+  const result = parser.parse(page);
+
+  t.is(result.type, tags.authenticationFailed);
 });
 
 test("identify session page", async t => {
-  const parser = new Parser();
   const page = await loadMockPage("session");
-  t.is(parser.parse(page).type, tags.session);
+
+  const parser = new Parser();
+  const result = parser.parse(page);
+
+  t.is(result.type, tags.session);
 });
