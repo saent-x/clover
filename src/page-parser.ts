d9e12ae985fe576ed993baea5c36f9eb49d203ab
Edosa Kelvin <edosakelvin@gmail.com>
2018-12-08 20:12:14 +0100

    Moved implementation of parseAdjacentSiblingValue to parse-page.ts beecause that's the only place it's used really

--- src/page-parser.ts
+++ src/page-parser.ts
@@ -1,9 +1,28 @@
 import cheerio = require("cheerio");
 import IPageParser from "./interfaces/ipage-parser";
 import PageType from "./page-type"
-import { parseAdjacentSiblingValue } from "./parse-util";
 
 const sessionParser: IPageParser = (function () {
+    function parseAdjacentSiblingValue($: any, key: string): string {
+        /* Todo: re-write this block, it's a tad messy */
+        let query = Array.from($("td.para1"))
+            .map(function (node: any) { node.children[0] })
+            .filter(function (node: any) {
+                const sanitize = function (value: string) { value.toLocaleLowerCase().trim(); }
+                return node && node.data ? sanitize(key) === sanitize(node.data) : false;
+            })
+            .map(function (node: any) {
+                let currentNode = node.parent;
+                /* keep moving until we hit an adjacent sibling */
+                do {
+                    currentNode = currentNode.next;
+                } while (currentNode.type != "tag");
+    
+                return currentNode.children[0].data;
+            });
+    
+        return query.length > 0 ? query[0] : null;
+    }
     function parseSessionId($: any): Number {
         const content = $("meta").last()[0].attribs.content;
         return Number.parseInt(content.slice(content.lastIndexOf("=") + 1));
